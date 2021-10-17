0de5ecaddc0f75f14a352fdf7b5e0accc67447b7
KingMarine <61825575+KingMarine-GH@users.noreply.github.com>
2021-10-18 01:08:17 +0800

    updated scripts

--- build.sh
+++ build.sh
@@ -4,12 +4,5 @@ rollup -c
 
 # build server
 cd ../server
+rm -r build
 tsc
-
-# copy builds to central build
-cd ..
-rm -r build || true
-mkdir build
-cp -r server/build build/server
-cp package.json build/server
-cp -r website/public build/public
