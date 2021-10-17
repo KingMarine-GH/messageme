2cf84a46bf2c83c22505bb7f350b4e60259d0023
KingMarine <61825575+KingMarine-GH@users.noreply.github.com>
2021-10-17 21:18:35 +0800

    made shell do everything instead

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
+# build website
+cd website
+rollup -c
+
+# build server
+cd ../server
+tsc
+
+# copy builds to central build
+cd ..
 rm -r build || true
 mkdir build
 cp -r server/build build/server
