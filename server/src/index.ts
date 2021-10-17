debbc2c66e8b66b618ca790798f54e9c7e13fc6e
KingMarine <61825575+KingMarine-GH@users.noreply.github.com>
2021-10-18 01:57:18 +0800

    added websockets

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,20 @@
 import * as dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: "../../.env" });
 
-import * as express from "express"
+import * as express from "express";
 
-export const app = express()
+export const app = express();
 
-app.listen(process.env.EXPRESS_PORT);
+app.listen(<number> <unknown> process.env.EXPRESS_PORT);
 
 app.get("/", (req, res) => {
     res.sendFile("/website/public/index.html", {root: "."});
 });
+
+import * as ws from "ws";
+export const server = new ws.Server({ port: <number> <unknown> process.env.WS_PORT });
+
+server.once("listening", () => {
+    console.log("WebSocket server online in port " + process.env.WS_PORT)
+})
